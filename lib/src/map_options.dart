97ca4f0136eff877fe73b6b87cf2cb5745f490de
supercaly <djonline96@gmail.com>
2020-07-30 09:36:28 +0200

    Changes to map_options.dart and add map_options_test.dart

--- lib/src/map_options.dart
+++ lib/src/map_options.dart
@@ -2,14 +2,39 @@ import 'package:flutter/material.dart';
 
 import 'latlng.dart';
 
+/// Represent a set of options used to
+/// customize a specific map.
 class MapOptions {
+  /// A pair of [LatLng] object defining the rectangular
+  /// bounding box of the map.
+  final List<LatLng> area;
+
+  /// A [LatLng] object defining the center of the map.
+  ///
+  /// This option must be used in conjunction with [zoom].
   final LatLng center;
+
+  /// Value of the map's zoom; used only with [center].
+  ///
+  /// His value must be in range [1,20].
   final double zoom;
+
+  /// Defines an image used as placeholder while the map
+  /// is downloaded form the web.
   final ImageProvider placeholder;
 
   MapOptions({
+    this.area,
     this.center,
     this.zoom,
     this.placeholder,
-  });
+  })  : assert(
+            (center == null && zoom == null && area != null) ||
+                (center != null && zoom != null && zoom >= 1 && zoom <= 20.0),
+            "When using center as map's position center and zoom must both be given. Zoom must be in range [1,20]"),
+        assert(area == null || (area != null && area.length == 2),
+            "When using area as map's position area must be a size 2 list of LatLng!");
+
+  bool get isCenter => center != null && zoom != null;
+  bool get isArea => area != null;
 }
---
+++ test/map_options_test.dart
@@ -0,0 +1,84 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fmaps/fmaps.dart';
+
+void main() {
+  group("MapOptions Test", ()
+  {
+    test("init with center and zoom", () {
+      final opt = MapOptions(
+        center: LatLng(0.0, 0.0),
+        zoom: 11.0,
+      );
+
+      expect(opt.center, isNotNull);
+      expect(opt.center, equals(LatLng(0.0, 0.0)));
+
+      expect(opt.zoom, isNotNull);
+      expect(opt.zoom, equals(11.0));
+
+      expect(opt.area, isNull);
+    });
+
+    test("init with area", () {
+      final opt = MapOptions(
+        area: [LatLng(0.0, 0.0), LatLng(0.0, 0.0),]
+      );
+
+      expect(opt.area, isNotNull);
+      expect(opt.area, hasLength(2));
+
+      expect(opt.center, isNull);
+      expect(opt.zoom, isNull);
+    });
+
+    test("init with null center or zoom throws exception", () {
+      try {
+        MapOptions();
+        fail("This should throw an assertion error!");
+      } catch (e) {
+        expect(e, isA<AssertionError>());
+      }
+
+      try {
+        MapOptions(
+          zoom: 10.0,
+        );
+        fail("This should throw an assertion error!");
+      } catch (e) {
+        expect(e, isA<AssertionError>());
+      }
+
+      try {
+        MapOptions(
+          center: LatLng(0.0, 0.0)
+        );
+        fail("This should throw an assertion error!");
+      } catch (e) {
+        expect(e, isA<AssertionError>());
+      }
+    });
+
+    test("init with null area throws exception", () {
+      try {
+        MapOptions();
+        fail("This should throw an assertion error!");
+      } catch (e) {
+        expect(e, isA<AssertionError>());
+      }
+
+      try {
+        MapOptions(
+          area: [
+            LatLng(0.0, 0.0),
+            LatLng(0.0, 0.0),
+            LatLng(0.0, 0.0),
+          ]
+        );
+        fail("This should throw an assertion error!");
+      } catch (e) {
+        expect(e, isA<AssertionError>());
+      }
+    });
+  });
+}
