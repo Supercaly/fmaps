abdae268737d15d0da40445ccaef2d47bbd64759
supercaly <djonline96@gmail.com>
2020-07-30 09:59:21 +0200

    Clamp latlng.dart parameters during constructor and add latlng_test.dart

--- lib/src/latlng.dart
+++ lib/src/latlng.dart
@@ -2,10 +2,22 @@ class LatLng {
   final double latitude;
   final double longitude;
 
-  LatLng(this.latitude, this.longitude);
+  LatLng(double latitude, double longitude)
+      : this.latitude = latitude.clamp(-90.0, 90.0),
+        this.longitude = longitude.clamp(-180.0, 180.0);
 
   @override
   String toString() {
     return 'LatLng{latitude: $latitude, longitude: $longitude}';
   }
+
+  @override
+  bool operator ==(Object other) {
+    return other is LatLng &&
+        this.latitude == other.latitude &&
+        this.longitude == other.longitude;
+  }
+
+  @override
+  int get hashCode => latitude.hashCode ^ longitude.hashCode;
 }
---
+++ test/latlng_test.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fmaps/fmaps.dart';
+
+void main() {
+  group("LatLng Test", () {
+    test("constructor clamps the values outside range", () {
+      final l1 = LatLng(0.0, 0.0);
+      final l2 = LatLng(-100.0, 200.0);
+      final l3 = LatLng(100.0, -200.0);
+
+      expect(l1.latitude, equals(0.0));
+      expect(l1.longitude, equals(0.0));
+
+      expect(l2.latitude, equals(-90.0));
+      expect(l2.longitude, equals(180.0));
+
+      expect(l3.latitude, equals(90.0));
+      expect(l3.longitude, equals(-180.0));
+    });
+
+    test("equals works correctly", () {
+      final l1 = LatLng(0.0, 0.0);
+      final l2 = LatLng(3.5, 12.6);
+      final l3 = LatLng(0.0, 0.0);
+
+      expect(l1, isNot(equals(l2)));
+      expect(l1, equals(l3));
+    });
+  });
+}
