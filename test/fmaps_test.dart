fad3eb448d4e99b6a77f75fbf09b0eef9dd46481
supercaly <djonline96@gmail.com>
2020-07-31 18:59:55 +0200

    Add widget tests

--- test/fmaps_test.dart
+++ test/fmaps_test.dart
@@ -1,1 +1,57 @@
-void main() {}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/painting/image_provider.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fmaps/fmaps.dart';
+import 'package:fmaps/src/fmaps_widget.dart';
+
+void main() {
+  group("FMaps Test", () {
+    testWidgets("create FMaps with null options throws an assertion error",
+        (tester) async {
+      try {
+        await tester.pumpWidget(FMaps(
+          options: null,
+        ));
+        fail("Create an FMaps with null options should not be possible!");
+      } on AssertionError catch (e) {
+        expect(e.toString(), contains("options != null"));
+      }
+    });
+
+    testWidgets(
+        "create FMaps with null provider throws an assertion error during build",
+        (tester) async {
+      await tester.pumpWidget(FMaps(
+        options: MapOptions(
+          center: LatLng(0.0, 0.0),
+          zoom: 19.0,
+        ),
+      ));
+      expect(tester.takeException(), isA<AssertionError>());
+    });
+
+    testWidgets("create FMaps show the map correctly", (tester) async {
+      await tester.pumpWidget(FMaps(
+        options: MapOptions(
+          center: LatLng(0.0, 0.0),
+          zoom: 19.0,
+        ),
+        provider: MockMapProvider(),
+      ));
+
+      await tester.pumpAndSettle();
+
+      expect(
+        tester.firstWidget<Image>(find.byType(Image)).image,
+        equals(AssetImage("assets/placeholder.png")),
+      );
+    });
+  });
+}
+
+class MockMapProvider extends MapProvider {
+  @override
+  ImageProvider getStaticMap(int width, int height, MapOptions options) {
+    return AssetImage("assets/placeholder.png");
+  }
+}
---
+++ test/marker_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fmaps/src/latlng.dart';
+import 'package:fmaps/src/marker.dart';
+
+void main() {
+  group("Marker Test", () {
+    test("markers with same content are qeuals", () {
+      final m1 = Marker(position: LatLng(0.0, 0.0), color: Colors.red);
+      final m2 = Marker(position: LatLng(12.0, 43.0), color: Colors.blue);
+      final m3 = Marker(position: LatLng(0.0, 0.0), color: Colors.red);
+
+      expect(m1, equals(m3));
+      expect(m1, isNot(equals(m2)));
+
+      expect(m2, isNot(equals(m1)));
+      expect(m2, isNot(equals(m3)));
+
+      expect(m3, equals(m1));
+      expect(m3, isNot(equals(m2)));
+    });
+  });
+}
